11d176e86bfa222a0f944cf37d9d90b81fb516ae
Paul Ksenofontov <mr.pavel.ksenofontov@gmail.com>
2023-01-06 11:51:28 +0300

    task done

---
+++ generator.py
@@ -0,0 +1,52 @@
+import types
+
+
+def flat_generator_1(list_of_lists):
+    for item in list_of_lists:
+        for i in item:
+            yield i
+
+
+
+def flat_generator_2(list_of_lists):
+    for i in list_of_lists:
+        if isinstance(i, list):
+            yield from flat_generator_2(i)
+        else:
+            yield i
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator_1(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator_1(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator_1(list_of_lists_1), types.GeneratorType)
+
+
+def test_4():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator_2(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator_2(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator_2(list_of_lists_2), types.GeneratorType)
---
+++ main.py
@@ -0,0 +1,8 @@
+from generator import test_2, test_4
+from iterator import test_1
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_4()
